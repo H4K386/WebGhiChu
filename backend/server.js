47105be863704c0ce6dd49d87da54a9c2931c7e0
H4K386 <nkhanhhuy10062006@gmail.com>
2026-09-14 17:07:39 +0700

    Tao branch Khoa - Huy - Hoi va khoi tao file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-//Tạo backend ở đây
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- frontend/app.jsx
+++
@@ -1,1 +0,0 @@
-//Tạo frontend trong đây
